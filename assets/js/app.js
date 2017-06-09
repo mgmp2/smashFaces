bb3cc658e530469f921f7eec3141532e27edf5d7
Miriam <miriammp1997@gmail.com>
2017-06-09 12:16:05 -0500

    create my object

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,32 @@
 'use strict';
+const render = (root) => { //Permite volver a pintar
+    root.empty();       //Primero vaciar
+    const wrapper = $('<div class="wrapper"></div>');
+    //Append de componentes
+    wrapper.append(Header( _ => {
+        render(root);
+    }));
 
-const state = {
+    // if(state.selectedStation == null ) {
+    //     wrapper.append(Search( _ => {
+    //         render(root);
+    //     }));
+    // }
+    // else {
+    //     wrapper.append(detailsStation( _ => {
+    //         render(root);
+    // }));
+    // }
+    root.append(wrapper);
+};
+
+// MY OBJECT
+const state  = {
     coders: null,
     selectedCoder: null
 }
+
+
 
 $(_ => {
     getJSON('peru.json', (err, json) => {
--- assets/js/utils/get-json.js
+++ assets/js/utils/get-json.js
@@ -1,6 +1,7 @@
 'use strict';
 
 
+
 const getJSON = (url, cb) => {
 
   const xhr = new XMLHttpRequest();
