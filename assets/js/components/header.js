4306c1b9ea7c6d2acd4b649ac8f5125cf5be8e86
Miriam Mendoza Pilco <miriammp1997@gmail.com>
2017-06-10 23:48:34 -0500

    add line in my tittle

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -3,6 +3,9 @@
 const Header = (update) => {
     const header = $('<header class="jumbotron"></header>');
     const titulo = $('<h1 class="text-center">Smash Faces </h1>');
+    const line = $('<hr>');
+
     header.append(titulo);
+    header.append(line);
     return header;
 }
